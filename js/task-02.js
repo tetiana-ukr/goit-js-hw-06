69a2f59b0ee16e63b1be82db409983b6daf33571
Tanya <pilipenkotanya75@gmail.com>
2023-05-21 10:39:40 +0300

    homework # 6 to complete exercise 2

--- js/task-02.js
+++ js/task-02.js
@@ -6,10 +6,30 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
-
+                 /*  1-й варіант*/
                 
+//const listEl = document.querySelector("#ingredients");
+//const markupList = ingredients.map((ingredient) => `<li class="item">${ingredient}</li>`).join("");
+//console.log(markupList);
+//listEl.innerHTML = markupList;
+
+                  /*   2-й варіант*/
+                 
 const listEl = document.querySelector("#ingredients");
-const markupList = ingredients.map((ingredient) => `<li class="item">${ingredient}</li>`).join("");
-console.log(markupList);
-listEl.innerHTML = markupList;
+
+const markupList = ingredients.map((ingredient) => {
+
+  const item = document.createElement("li");
+
+  item.textContent = ingredient;
+
+  item.classList.add("item");
+
+    return item;
+})
+
+listEl.after(...markupList);
+
+//console.log(markupList);
+
 
